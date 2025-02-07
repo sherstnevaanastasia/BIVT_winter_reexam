c1f1d9e996e4f073efb0eda276003348436c6fe3
Mark <112826355+NemirovichMark@users.noreply.github.com>
2025-02-07 11:50:35 +0300

    Add files via upload

---
+++ Variant_10.cs
@@ -0,0 +1,95 @@
+﻿//// Variant_10
+
+//namespace Exam
+//{
+//    public class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            // Task_1:
+//            // Input: S = 4, A = 6, N = 22
+//            // Output: time = 18
+
+//            // Task_2:
+//            // Input: GPU = 4, RAM = 6, CPU = 22
+//            // Output: fps = 13,200000000000003
+
+//            // Task_3:
+//            // Input:
+//            /*   23,   7, -13,  24, -21,  18, 
+//                  2,   0,  12, -16, -20, -17, 
+//                 22,  21,  -6,  19, -22,  -4, 
+//                 -13, 13,  18, -15, -20,  -2, 
+//                  3,   7,   1, -20,  22,  -8, 
+//                -22, -11,  13,  -2,   0, -14 */
+//            // Output:
+//            /*   23,   7, -13,  24, -21,  18, 
+//                  2,   0,  12, -16, -20, -17, 
+//                 22,  21,  -6,  19, -22,  -4, 
+//                 22,  21,  18,  19, -22,  -4, 
+//                  2,   0,   1, -16, -20, -17, 
+//                 23,   7,  13,  24, -21,  18 */
+
+//            // Task_4:
+//            // Input:
+//            /*   17,  17,   2, -10,  -1, -20 */
+//            // Output:
+//            /*    2, -10, -20 */
+
+//            // Task_5:
+//            // Input:
+//            /*   23,   7, -13,  24, -21,  18, 
+//                  2,   0,  12, -16, -20, -17, 
+//                 22,  21,  -6,  19, -22,  -4, 
+//                -13,  13,  18, -15, -20,  -2, 
+//                  3,   7,   1, -20,  22,  -8, 
+//                -22, -11,  13,  -2,   0, -14 */
+
+//            /*   17,  17,   2, -10,  -1, -20 */
+//            // Output 1:
+//            /*   23,   7, -13,  24, -21,  18, 
+//                  2,   0,  12, -16, -20, -17, 
+//                 22,  21,  -6,  19, -22,  -4, 
+//                -13,  13,  18, -15, -20,  -2, 
+//                  3,   7,   1, -20,  22,  -8, 
+//                -22, -11,  13,  -2,   0, -14 */
+
+//            /*  -20, -10,  -1,   2,  17,  17, -15, -14,  -6,   0,  22,  23 */
+//            // Output 2:
+//            /*   23,   7, -13,  24, -21,  18, 
+//                  2,   0,  12, -16, -20, -17, 
+//                 22,  21,  -6,  19, -22,  -4, 
+//                -13,  13,  18, -15, -20,  -2, 
+//                  3,   7,   1, -20,  22,  -8, 
+//                -22, -11,  13,  -2,   0, -14 */
+//            /*   17,  17,   2,  -1, -10, -20,  23,  22,   0,  -6, -14, -15 */
+
+//        }
+//        public double Task_1(double S, double A, double N)
+//        {
+//            return 0;
+//        }
+//        public double Task_2(double GPU, double RAM, double CPU)
+//        {
+//            return 0;
+//        }
+//        public void Task_3(int[,] M)
+//        {
+
+//        }
+//        public void Task_4(ref int[] A)
+//        {
+
+//        }
+//        public int[] GetArraySequence(int[] array, bool isEven) { return default(int[]); }
+//        public void Task_5(ref int[,] M, ref int[] A, SortArray Op)
+//        {
+
+//        }
+//        public delegate void SortArray(int[] array);
+//        public void SortAscending(int[] array) { }
+//        public void SortDescending(int[] array) { }
+//        public int[] Concat(int[] array1, int[] array2) { return default(int[]); }
+
+//    }
+//}
---
+++ Variant_9.cs
@@ -0,0 +1,101 @@
+﻿//// Variant_9
+//namespace Exam
+//{
+//    public class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            // Task_1:
+//            // Input: T = 4, D = 6, N = 22
+//            // Output: time = 83885792
+
+//            // Task_2:
+//            // Input: speedLimit = 4, motorLimit = 6, turnsPerSec = 22
+//            // Output: time = 5,899999999999999
+
+//            // Task_3:
+//            // Input:
+//            /*   23,   7, -13,  24, -21,  18, 
+//                  2,   0,  12, -16, -20, -17, 
+//                 22,  21,  -6,  19, -22,  -4, 
+//                 -13, 13,  18, -15, -20,  -2, 
+//                  3,   7,   1, -20,  22,  -8, 
+//                -22, -11,  13,  -2,   0, -14 */
+//            // Output:
+//            /*   23,   0,  -2,  13, -11,  18, 
+//                 -8,   0, -20,   1, -20,   3, 
+//                 -2, -20,  -6,  19,  13, -13, 
+//                 -4, -22,  18, -15,  21,  22, 
+//                -17,   7, -16,  12,  22,   2, 
+//                -22, -21,  24, -13,   7, -14 */
+
+//            // Task_4:
+//            // Input:
+//            /*   17,  17,   2, -10,  -1, -20 */
+//            // Output:
+//            /*  -20, -10,  -1 */
+
+//            // Task_5:
+//            // Input:
+//            /*   23,   7, -13,  24, -21,  18, 
+//                  2,   0,  12, -16, -20, -17, 
+//                 22,  21,  -6,  19, -22,  -4, 
+//                -13,  13,  18, -15, -20,  -2, 
+//                  3,   7,   1, -20,  22,  -8, 
+//                -22, -11,  13,  -2,   0, -14 */
+
+//            /*   17,  17,   2, -10,  -1, -20 */
+//            // Output 1:
+//            /*   23,   7, -13, -20, -21,  18, 
+//                  2,   0,  12, -16, -20, -17, 
+//                 22,  21,  -6,  19, -22,  -4, 
+//                -13,  13,  18, -15, -20,  -2, 
+//                  3,   7,   1, -20,  22,  -8, 
+//                 17, -11,  13,  -2,   0, -14 */
+
+//            /*  -20, -10,  -1,   2,  17,  17 */
+//            // Output 2:
+//            /*   23,   7, -13,  17, -21,  18, 
+//                  2,   0,  12, -16, -20, -17, 
+//                 22,  21,  -6,  19, -22,  -4, 
+//                -13,  13,  18, -15, -20,  -2, 
+//                  3,   7,   1, -20,  22,  -8, 
+//                -20, -11,  13,  -2,   0, -14 */
+
+//            /*   17,  17,   2,  -1, -10, -20 */
+
+//        }
+//        public double Task_1(double T, double D, double N)
+//        {
+//            return 0;
+//        }
+//        public double Task_2(double speedLimit, double motorLimit, double turnsPerSec)
+//        {
+//            return 0;
+//        }
+//        public void Task_3(int[,] M)
+//        {
+
+//        }
+//        public void Task_4(ref int[] A)
+//        {
+
+//        }
+//        public int[] GetNegativeSubArray(int[] array) { return default(int[]); }
+//        public void Task_5(ref int[,] M, ref int[] A, SortArray Op)
+//        {
+
+//        }
+//        public delegate void SortArray(int[] array);
+//        public void SortAscending(int[] array) { }
+//        public void SortDescending(int[] array) { }
+//        public void FindUpperPartMaxIndex(int[,] matrix, out int maxRow, out int maxCol)
+//        {
+//            maxRow = 0; maxCol = 0;
+//        }
+//        public void FindLowerPartMinIndex(int[,] matrix, out int minRow, out int minCol)
+//        {
+//            minRow = 0; minCol = 0;
+//        }
+//    }
+//}
